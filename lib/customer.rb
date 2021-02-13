8f17a9bd3f6f44e85cfd7c11c90b8dcc4a599068
Mike Nanto <mike@thecowboymechanic.com>
2021-02-13 06:27:41 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,16 @@
 class Customer
   attr_acccessor :name, :age 
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
