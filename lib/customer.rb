deec0872013154eeaab596ccdd46a987cce72936
Mike Nanto <mike@thecowboymechanic.com>
2021-02-13 06:37:21 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,12 +13,13 @@ class Customer
     Meal.all.select do |meal|
       meal.customer == self
     end
-  end
+  end 
   
   def waiters
     meals.map do |meal|
       meal.waiter
     end
+  end 
     
   def self.all
     @@all
