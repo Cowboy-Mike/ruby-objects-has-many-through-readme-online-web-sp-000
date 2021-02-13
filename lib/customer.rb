c6bf98192d8eadb2790baa70c0b2fd2e6d4a200f
Mike Nanto <mike@thecowboymechanic.com>
2021-02-13 06:42:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -9,6 +9,10 @@ class Customer
     @@all << self 
   end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
   def meals
     Meal.all.select do |meal|
       meal.customer == self
